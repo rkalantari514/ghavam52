5a5e508d7b1297d8d789acb2c504b04322c92734
rkalantari <rkalantari514@gmail.com>
2024-06-05 03:52:07 -0700

    initial

--- process/admin.py
+++ process/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from process.models import RemittanceRow
+from process.models import RemittanceRow, Remittance
 
 
 # Register your models here.
@@ -13,5 +13,15 @@ class RemittanceRowAdmin(admin.ModelAdmin):
     # list_per_page = 20
     class Meta:
         model = RemittanceRow
+class RemittanceAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
+    # list_display = ['__str__','name1', 'name2']
+    # list_filter =['name1', 'name2']
+    # list_editable =['name1', 'name2']
+    # search_fields =['name1', 'name2']
+    # list_per_page = 20
+    class Meta:
+        model = Remittance
 
 admin.site.register(RemittanceRow, RemittanceRowAdmin)
+admin.site.register(Remittance, RemittanceAdmin)
--- process/models.py
+++ process/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 
 # Create your models here.
-class RemittanceRow(models.Model):
+class Remittance(models.Model):
     date=models.DateField(default=datetime.date.today)
 
     #
@@ -11,6 +11,23 @@ class RemittanceRow(models.Model):
     class Meta:
         verbose_name = 'حواله انبار'
         verbose_name_plural = 'حواله های انبار'
+
+
+
+
+    def __str__(self):
+        return str(self.id)
+
+class RemittanceRow(models.Model):
+    numberin=models.IntegerField(default=0,verbose_name='تعداد ورود', null=True, blank=True)
+    remittance=models.ForeignKey(Remittance, blank=True, null=True, on_delete=models.CASCADE ,verbose_name='حواله')
+
+
+    #
+    # master_image = models.ImageField(upload_to=upload_about_us_image_path, null=True, blank=True, verbose_name='تصویر اصلی')
+    class Meta:
+        verbose_name = 'ردیف حواله انبار'
+        verbose_name_plural = 'ردیف های حواله انبار'
 
 
 
